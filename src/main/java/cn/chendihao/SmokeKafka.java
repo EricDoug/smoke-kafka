a5268dd22846bae2c95e50d949af8e72e08dadd9
tobe <tobeg3oogle@gmail.com>
2015-03-16 18:07:35 +0800

    Initialize maven project

---
+++ src/main/java/cn/chendihao/SmokeKafka.java
@@ -0,0 +1,19 @@
+package cn.chendihao;
+
+/**
+ * Kafka example code for smoke test.
+ */
+public class SmokeKafka {
+    
+    public static void main(String[] argv) {
+
+        System.out.println("Start smoke kafka");
+        
+        
+        
+        System.out.println("Stop smoke kafka");
+        
+    }
+
+}
+
